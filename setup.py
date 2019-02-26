935fd52ad7fc028d4b9e1aff801a65db84d1a6e4
Michael Recachinas <mgr3yp@virginia.edu>
2019-02-26 16:19:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,25 @@
 from distutils.core import setup, Extension
 
 
-#with open("./README.rst", "r") as readme_file:
-#	LONG_DESCRIPTION = readme_file.read()
-LONG_DESCRIPTION = ""
+with open("./README.rst", "r") as readme_file:
+    LONG_DESCRIPTION = readme_file.read()
 
 setup(
-	name='Hamming',
-	version='1.0',
-	description='',
-	long_description=LONG_DESCRIPTION,
+    name='Hamming',
+    version='1.0',
+    description='',
+    url='https://github.com/mrecachinas/hex-hamming.git'
+    long_description=LONG_DESCRIPTION,
     ext_modules=[
-    	Extension(
-    		name="hamming",
-    		sources=["hamming/python_hamming.cc", "hamming/hamming.cc"],
-    		extra_compile_args=["-O0", "-mavx512f"],
-    		include_dirs=['./hamming/'],
-    		language="c++",
-    	)
+        Extension(
+            name="hamming",
+            sources=["hamming/python_hamming.cc", "hamming/hamming.cc"],
+
+            # TODO: Figure out why -O2 causes an illegal hardware instruction
+            extra_compile_args=["-O0", "-mavx512f"],
+
+            include_dirs=['./hamming/'],
+            language="c++",
+        )
     ],
 )
