c6cf42564c2884cdc7d9fa9cc7cea6a29bdbe889
Michael Recachinas <mgr3yp@virginia.edu>
2019-03-02 10:55:35 +0000

    Merge hexhamming.h and python_hexhamming.h/c

--- hexhamming/python_hexhamming.c
+++ hexhamming/python_hexhamming.c
@@ -1,6 +1,50 @@
 #include "python_hexhamming.h"
 
 /**
+ * Returns the hamming distance of the binary between two hexadecimal strings
+ *
+ * @param a    hexadecimal char array
+ * @param b    hexadecimal char array
+ * @param a_string_length length of `a`
+ * @param b_string_length length of `b`
+ * @return      the number of bits different between the hexadecimal strings
+ */
+inline int hamming_distance(
+		const char* a,
+		const char* b,
+		size_t a_string_length,
+		size_t b_string_length
+	) {
+		// if both strings are the same, short circuit
+		// and return 0
+    if (strcmp(a, b) == 0) {
+      return 0;
+    }
+
+    int result = 0;
+    int val1, val2;
+    size_t i;
+    for (i = 0; i < a_string_length; ++i) {
+				// check to make sure all characters are valid
+				// hexadecimal in both strings
+        if ((a[i] > 'F' && a[i] < 'a') || (a[i] > 'f') ||
+				    (b[i] > 'F' && b[i] < 'a') || (b[i] > 'f')) {
+            return -1;
+        }
+
+				// Convert the hex ascii char to its actual hexadecimal value
+				// e.g., '0' = 0, 'A' = 10, etc.
+				// Note: this is case INSENSITIVE
+        val1 = (a[i] > '9') ? (a[i] &~ 0x20) - 'A' + 10: (a[i] - '0');
+        val2 = (b[i] > '9') ? (b[i] &~ 0x20) - 'A' + 10: (b[i] - '0');
+
+        result += LOOKUP_MATRIX[val1][val2];
+    }
+
+    return result;
+}
+
+/**
  * Python interface for `hamming_distance`
  *
  * @param self      Python `self` object
