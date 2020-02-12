187bd6e02389e52985c3368abf9e75e9c0c99376
Michael Recachinas <mgr3yp@virginia.edu>
2020-02-12 20:21:47 +0000

    ref: Parametrize tests and add cases for check_hexstrings_within_dist

--- test/test_hexhamming.py
+++ test/test_hexhamming.py
@@ -1,79 +1,67 @@
 #!/usr/bin/env python
 import pytest
-from hexhamming import hamming_distance
+from hexhamming import check_hexstrings_within_dist, hamming_distance
 
 ############################
 # hamming_distance tests
 ############################
 
-@pytest.mark.benchmark(group="12-bit same")
-def test_hamming_distance_same(benchmark):
-    result = benchmark(hamming_distance, *("abc", "abc"))
-    assert result == 0
+
+@pytest.mark.parametrize(
+    "hex1,hex2,expected",
+    (
+        ("abc", "abc", 0),
+        ("000", "001", 1),
+        ("", "", 0),
+        ("f" * 64, "0" * 64, 256),
+        ("f" * 64, "f" * 64, 0),
+        ("0" * 64, "0" * 64, 0),
+        ("f" * 10000, "0" * 10000, 40000),
+        ("f" * 10000, "f" * 10000, 0),
+    ),
+)
+def test_hamming_distance(hex1, hex2, expected):
+    assert hamming_distance(hex1, hex2) == expected
 
 
-@pytest.mark.benchmark(group="12-bit different")
-def test_hamming_distance_different(benchmark):
-    result = benchmark(hamming_distance, *("000", "001"))
-    assert result == 1
+@pytest.mark.parametrize(
+    "hex1,hex2,exception,msg",
+    (
+        ("abc", 3, ValueError, "error occurred while parsing arguments"),
+        ("abc", "a", ValueError, "strings are NOT the same length"),
+        ("lol", "foo", ValueError, "hex string contains invalid char"),
+    ),
+)
+def test_hamming_distance_errors(hex1, hex2, exception, msg):
+    with pytest.raises(exception) as excinfo:
+        _ = hamming_distance(hex1, hex2)
+    assert msg in str(excinfo.value)
 
 
-@pytest.mark.benchmark(group="empty string")
-def test_hamming_distance_empty(benchmark):
-    result = benchmark(hamming_distance, *("", ""))
-    assert result == 0
+@pytest.mark.parametrize(
+    "hex1,hex2,max_dist,expected",
+    (
+        ("000abcdef", "011abcdef", 3, True),
+        ("1f0abcdef", "011abcdef", 3, False),
+        ("011abcdef", "011abcdef", 1000, True),
+    ),
+)
+def test_check_hexstrings_within_dist(hex1, hex2, max_dist, expected):
+    assert check_hexstrings_within_dist(hex1, hex2, max_dist) == expected
 
 
-def test_hamming_distance_invalid():
-    with pytest.raises(ValueError):
-        hamming_distance("abc", 3)
-
-
-def test_hamming_distance_different_len():
-    with pytest.raises(ValueError):
-        hamming_distance("abc", "a")
-
-
-def test_hamming_distance_invalid():
-    with pytest.raises(ValueError):
-        hamming_distance("lol", "foo")
-
-
-@pytest.mark.benchmark(group="256-bit different")
-def test_hamming_distance_max_len_different(benchmark):
-    a = "f" * 64
-    b = "0" * 64
-    result = benchmark(hamming_distance, *(a, b))
-    assert result == len(a) * 4
-
-
-@pytest.mark.benchmark(group="256-bit same f")
-def test_hamming_distance_max_len_same_f(benchmark):
-    a = "f" * 64
-    b = "f" * 64
-    result = benchmark(hamming_distance, *(a, b))
-    assert result == 0
-
-
-@pytest.mark.benchmark(group="256-bit same 0")
-def test_hamming_distance_max_len_same_0(benchmark):
-    a = "f" * 64
-    b = "f" * 64
-    result = benchmark(hamming_distance, *(a, b))
-    assert result == 0
-
-
-@pytest.mark.benchmark(group="4000-bit different")
-def test_hamming_distance_stress_test_len_different(benchmark):
-    a = "f" * 1000
-    b = "0" * 1000
-    result = benchmark(hamming_distance, *(a, b))
-    assert result == len(a) * 4
-
-
-@pytest.mark.benchmark(group="4000-bit same")
-def test_hamming_distance_stress_test_len_same(benchmark):
-    a = "f" * 1000
-    b = "f" * 1000
-    result = benchmark(hamming_distance, *(a, b))
-    assert result == 0
+@pytest.mark.parametrize(
+    "hex1,hex2,max_dist,exception,msg",
+    (
+        ("000abcdef", "011abcdef", None, ValueError, "error occurred while parsing arguments"),
+        ("000abcdef", "011abcdef", "HELLO", ValueError, "error occurred while parsing arguments"),
+        ("000abcdef", "011abcdef", -1, ValueError, "`max_dist` must be >0"),
+        ("000abcdef", "011abcdzz", 3, ValueError, "hex string contains invalid char"),
+        ("1f0abcdef", 3, 3, ValueError, "error occurred while parsing arguments"),
+        ("011abcdef", "00", 3, ValueError, "strings are NOT the same length"),
+    ),
+)
+def test_check_hexstrings_within_dist(hex1, hex2, max_dist, exception, msg):
+    with pytest.raises(exception) as excinfo:
+        _ = check_hexstrings_within_dist(hex1, hex2, max_dist)
+    assert msg in str(excinfo.value)
