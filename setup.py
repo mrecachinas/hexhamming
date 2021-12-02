cd658f2be4e3c63c1e50179ad2330e3716eff827
Alexander Piskun <bigcat88@icloud.com>
2021-12-02 12:34:22 +0200

    added `keywords` and `zip_safe` values. moved author/email to right location.

--- setup.py
+++ setup.py
@@ -14,11 +14,13 @@ setup(
     ext_modules=[
         Extension(
             name="hexhamming",
-            author="Michael Recachinas",
-            author_email="m.recachinas@gmail.com",
             sources=["hexhamming/python_hexhamming.cc"],
             extra_compile_args=["-march=native"],
             language="c++11",
         )
     ],
+    author="Michael Recachinas",
+    author_email="m.recachinas@gmail.com",
+    keywords="hamming distance simd",
+    zip_safe=False,
 )
